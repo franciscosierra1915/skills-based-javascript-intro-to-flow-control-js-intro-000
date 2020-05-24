a9aecdff774e1cd9308318eb7c136805a3887d22
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 19:26:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,26 +19,29 @@ return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager
 }
 
 function switchAge(age) {
-swtich(age) {
-  case 13:
-    return "You are a teenager"
-    case 14:
-      return "You are a teenager"
-      case 14: 
-        return "You are a teenager"
-        case 15: 
-          return "You are a teenager"
-          case 16: 
-            return "You are a teenager"
-            case 17: 
-              return "You are a teenager"
-              case 18: 
-                return "You are a teenager"
-                case 19:
-                  return "You are a teenager"
-                  default:
-                  return "You have an age"
-            
-}
+switch(age) {
+case "13":
+console.log("You are a teenager");
+break;
+case "14":
+console.log("You are a teenager");
+break;
+case "15":
+console.log("You are a teenager");
+break;
+case "16":
+console.log("You are a teenager");
+break;
+case "17":
+console.log("You are a teenager");
+break;
+case "18":
+console.log("You are a teenager");
+break;
+case "19":
+console.log("You are a teenager");
+break;
+default:
+console.log("You have an age")
 
 }
